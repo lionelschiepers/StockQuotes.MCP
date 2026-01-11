67aa2c9a0f88ecc99e4a5606c9bb2ef6bdeed630
Lionel Schiepers <lionel_schiepers@hotmail.com>
2026-01-11 18:46:26 +0100

    ignore tests for linting

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import prettierConfig from 'eslint-config-prettier';
 export default [
   {
     files: ['**/*.ts'],
-    ignores: ['dist/**/*', 'node_modules/**/*'],
+    ignores: ['dist/**/*', 'node_modules/**/*', 'tests/**/*'],
     languageOptions: {
       parser: typescriptParser,
       parserOptions: {
